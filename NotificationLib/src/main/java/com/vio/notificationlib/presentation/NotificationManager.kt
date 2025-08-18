e127b46b26db28824915d49ef4f1663035eeb929
duyvv <duyvv@icebear.com.vn>
2025-08-18 16:30:29 +0700

    update ui notification

--- NotificationLib/src/main/java/com/vio/notificationlib/presentation/NotificationManager.kt
+++ NotificationLib/src/main/java/com/vio/notificationlib/presentation/NotificationManager.kt
@@ -8,17 +8,24 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.util.Log
+import android.view.View
 import android.widget.RemoteViews
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.vio.notificationlib.R
 import com.vio.notificationlib.domain.entities.NotificationConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 class NotificationManager(
     private val context: Context,
@@ -44,41 +51,56 @@ class NotificationManager(
             }
             notificationManager.createNotificationChannel(channel)
             val createdChannel = notificationManager.getNotificationChannel(CHANNEL_ID)
-            Log.d(TAG, "Notification channel created: $CHANNEL_ID, enabled=${createdChannel?.importance != NotificationManager.IMPORTANCE_NONE}")
+            Log.d(
+                TAG,
+                "Notification channel created: $CHANNEL_ID, enabled=${createdChannel?.importance != NotificationManager.IMPORTANCE_NONE}"
+            )
         }
     }
 
     fun showNotification(config: NotificationConfig) {
-        Log.d(TAG, "Notification triggered at ${dateFormat.format(Date())}: id=${config.id}, title=${config.title}, body=${config.body}, targetFeature=${config.targetFeature}, customLayout=${config.customLayout}, notificationType=${config.notificationType}")
-
-        when (config.notificationType) {
-            "FULLSCREEN" -> createNotificationStandard(config, true)
-            else -> createNotificationStandard(config, false)
+        Log.d(
+            TAG,
+            "Notification triggered at ${dateFormat.format(Date())}: id=${config.id}, title=${config.title}, body=${config.body}, targetFeature=${config.targetFeature}, customLayout=${config.customLayout}, notificationType=${config.notificationType}"
+        )
+        CoroutineScope(Dispatchers.Main).launch {
+            when (config.notificationType) {
+                "FULLSCREEN" -> createNotificationStandard(config, true)
+                else -> createNotificationStandard(config, false)
+            }
         }
     }
 
-    private fun createNotificationStandard(config: NotificationConfig, isFullscreen: Boolean) {
+    suspend fun createNotificationStandard(config: NotificationConfig, isFullscreen: Boolean) {
         Log.d(TAG, "createNotificationStandard: isFullscreen=$isFullscreen")
         val notificationContent = config.title
         val notificationDescription = config.body
-
+        val bitmap = loadBitmapWithFallback(context, config.imageUrl)
         // Create custom views
         val view = RemoteViews(context.packageName, R.layout.notification_layout).apply {
             setTextViewText(R.id.txtContentNoti, notificationContent)
             setOnClickPendingIntent(R.id.llNotiParent, getPendingIntentActivity(config))
+            if (bitmap != null) setImageViewBitmap(R.id.img, bitmap)
+            setViewVisibility(R.id.img, if (bitmap != null) View.VISIBLE else View.GONE)
         }
 
-        val expandedView = RemoteViews(context.packageName, R.layout.notification_layout_expanded).apply {
-            setTextViewText(R.id.txtContentNoti, notificationContent)
-            setTextViewText(R.id.txtDescriptionNoti, notificationDescription)
-            setOnClickPendingIntent(R.id.llNotiParent, getPendingIntentActivity(config))
-        }
+        val expandedView =
+            RemoteViews(context.packageName, R.layout.notification_layout_expanded).apply {
+                setTextViewText(R.id.txtContentNoti, notificationContent)
+                setTextViewText(R.id.txtDescriptionNoti, notificationDescription)
+                setOnClickPendingIntent(R.id.llNotiParent, getPendingIntentActivity(config))
+                if (bitmap != null) setImageViewBitmap(R.id.img, bitmap)
+                setViewVisibility(R.id.img, if (bitmap != null) View.VISIBLE else View.GONE)
+            }
 
-        val headerView = RemoteViews(context.packageName, R.layout.notification_layout_header).apply {
-            setTextViewText(R.id.txtContentNoti, notificationContent)
-            setTextViewText(R.id.txtDescriptionNoti, notificationDescription)
-            setOnClickPendingIntent(R.id.llNotiParent, getPendingIntentActivity(config))
-        }
+        val headerView =
+            RemoteViews(context.packageName, R.layout.notification_layout_header).apply {
+                setTextViewText(R.id.txtContentNoti, notificationContent)
+                setTextViewText(R.id.txtDescriptionNoti, notificationDescription)
+                setOnClickPendingIntent(R.id.llNotiParent, getPendingIntentActivity(config))
+                if (bitmap != null) setImageViewBitmap(R.id.img, bitmap)
+                setViewVisibility(R.id.img, if (bitmap != null) View.VISIBLE else View.GONE)
+            }
 
         val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setStyle(NotificationCompat.DecoratedCustomViewStyle())
@@ -92,13 +114,14 @@ class NotificationManager(
 
         // Chỉ sử dụng fullScreenPendingIntent khi thiết bị khóa
         if (isFullscreen && isDeviceLocked(context)) {
-            val fullScreenIntent = Intent(context, FullscreenNotificationActivity::class.java).apply {
-                putExtra("schedule_data", config)
-                putExtra("title", config.title)
-                putExtra("body", config.body)
-                config.targetFeature?.let { putExtra("target_feature", it) }
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-            }
+            val fullScreenIntent =
+                Intent(context, FullscreenNotificationActivity::class.java).apply {
+                    putExtra("schedule_data", config)
+                    putExtra("title", config.title)
+                    putExtra("body", config.body)
+                    config.targetFeature?.let { putExtra("target_feature", it) }
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                }
             val fullScreenPendingIntent = PendingIntent.getActivity(
                 context, config.id, fullScreenIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
@@ -119,7 +142,12 @@ class NotificationManager(
 
         try {
             notificationManager.notify(config.id, notificationBuilder.build())
-            Log.d(TAG, "Notification sent: id=${config.id}, isFullscreen=$isFullscreen at ${dateFormat.format(Date())}")
+            Log.d(
+                TAG,
+                "Notification sent: id=${config.id}, isFullscreen=$isFullscreen at ${
+                    dateFormat.format(Date())
+                }"
+            )
         } catch (e: SecurityException) {
             Log.e(TAG, "SecurityException: Missing permissions or other security issue", e)
         } catch (e: Exception) {
@@ -132,6 +160,7 @@ class NotificationManager(
         val keyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
         return keyguardManager.isKeyguardLocked
     }
+
     private fun getPendingIntentActivity(config: NotificationConfig): PendingIntent? {
 
         return config.activityClassName.let {
@@ -148,5 +177,38 @@ class NotificationManager(
 
     companion object {
         private const val TAG = "NotificationManager"
+    }
+
+    suspend fun loadBitmapWithFallback(
+        context: Context,
+        url: String,
+        fallbackDomain: String = "https://photos.lordeaglesoftware.com/",
+        fallbackBaseUrl: String = "http://64.176.221.209/"
+    ): Bitmap? = withContext(Dispatchers.IO) {
+        try {
+            // thử load với URL gốc
+            Glide.with(context)
+                .asBitmap()
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .submit()
+                .get()
+        } catch (e: Exception) {
+            Log.d(TAG, "loadBitmapWithFallback: ${e.message}")
+            if (url.contains(fallbackDomain)) {
+                val fallbackUrl = url.replace(fallbackDomain, fallbackBaseUrl)
+                try {
+                    Glide.with(context)
+                        .asBitmap()
+                        .load(fallbackUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.DATA)
+                        .submit()
+                        .get()
+                } catch (e2: Exception) {
+                    Log.d(TAG, "loadBitmapWithFallback: ${e2.message}")
+                    null
+                }
+            } else null
+        }
     }
 }
